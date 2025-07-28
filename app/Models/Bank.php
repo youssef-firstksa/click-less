345e908e40f8bb3a36eae2f73ca7f60312dc2170
Youssef Ahmed <youssefahmedsup@gmail.com>
2025-07-28 17:29:58 +0300

    update

--- app/Models/Bank.php
+++ app/Models/Bank.php
@@ -2,10 +2,14 @@
 
 namespace App\Models;
 
+use Astrotomic\Translatable\Translatable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Bank extends Model
 {
-    use HasFactory;
+    use HasFactory, Translatable;
+
+    public $translatedAttributes = ['title', 'slug', 'details'];
+    protected $fillable = ['color', 'logo', 'img', 'ai_key'];
 }
--- app/Models/BankTranslation.php
+++ app/Models/BankTranslation.php
@@ -10,4 +10,5 @@ class BankTranslation extends Model
     use HasFactory;
 
     public $timestamps = false;
+    public $fillable = ['title', 'slug', 'details'];
 }
